3c55d13337ce3c64c1af66ef090ed1a3fb99c42c
Dusan Odalovic <dodalovic@gmail.com>
2020-07-15 10:00:01 +0200

    Add some documentation

--- src/main/kotlin/com/odalovic/graphql/OrdersApi.kt
+++ src/main/kotlin/com/odalovic/graphql/OrdersApi.kt
@@ -8,25 +8,30 @@ import org.eclipse.microprofile.graphql.*
 class OrdersApi(private val orderService: OrderService) {
 
     @Query("order")
-    fun getOrderById(@Name("orderId") orderId: String) = orderService.findById(orderId)
-        ?.let { order ->
-            Order(
-                order.id ?: error("Order without ID!!!"),
-                order.items.map { item ->
-                    OrderItem(
-                        item.id ?: error("Order item without ID!!!"),
-                        item.productName,
-                        item.quantity
-                    )
-                })
-        }
+    @Description("Returns the order by given ID, or null if the order doesn't exist")
+    fun getOrderById(@Description("Unique order identifier") @Name("orderId") orderId: String) =
+        orderService.findById(orderId)
+            ?.let { order ->
+                Order(
+                    order.id ?: error("Order without ID"),
+                    order.items.map { item ->
+                        OrderItem(
+                            item.id ?: error("Order item without ID!!!"),
+                            item.productName,
+                            item.quantity
+                        )
+                    })
+            }
 
     fun items(@Source orderEntity: OrderEntity) =
         orderEntity.items.map { OrderItem(it.id ?: error("Order without ID!!!"), it.productName, it.quantity) }
 
     @Mutation("createOrder")
-    fun createOrder(newOrder: NewOrder) = orderService.createOrder(newOrder)
+    @Description("Creates a new order comprising given items")
+    fun createOrder(@Description("New order definition") newOrder: NewOrder) = orderService.createOrder(newOrder)
 
     @Mutation("deleteOrderItem")
-    fun deleteOrderItem(orderItemId: String) = orderService.deleteOrderItem(orderItemId)
+    @Description("Deletes particular order item by given item ID")
+    fun deleteOrderItem(@Description("Unique order item identifier") orderItemId: String) =
+        orderService.deleteOrderItem(orderItemId)
 }
--- src/main/kotlin/com/odalovic/graphql/apiModels.kt
+++ src/main/kotlin/com/odalovic/graphql/apiModels.kt
@@ -1,10 +1,27 @@
 package com.odalovic.graphql
 
-data class OrderItem(val id: String, val product: String, val quantity: Int)
-data class Order(val id: String, val items: List<OrderItem>? = null)
+import org.eclipse.microprofile.graphql.Description
+
+@Description("Order item output definition")
+data class OrderItem(
+    @field:Description("Order item ID") val id: String,
+    @field:Description("Product code") val product: String,
+    @field:Description("Product quantity") val quantity: Int
+)
+
+@Description("Order output definition")
+data class Order(
+    @field:Description("Order ID") val id: String,
+    @field:Description("Order items") val items: List<OrderItem>? = null
+)
 
 @NoArgConstructor
-data class NewOrderItem(var product: String, var quantity: Int)
+@Description("Order item definition")
+data class NewOrderItem(
+    @field:Description("Product code") var product: String,
+    @field:Description("Product quantity") var quantity: Int
+)
 
 @NoArgConstructor
-data class NewOrder(var items: List<NewOrderItem>)
+@Description("New order definition")
+data class NewOrder(@Description("Order items comprising order") var items: List<NewOrderItem>)
